8e3e88e2f0d330ce4124450cae0ae1c045478d1e
ArunKumar000777 <arunashok1020@gmail.com>
2023-05-25 17:12:48 +0530

    about page font changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,29 +5,31 @@ import { motion } from "framer-motion";
 const About = () => {
     return (
         <motion.div
-            className="mt-10 bg-light pt-14"
+            className=" bg-light pt-14"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
             {/* section1 */}
-            <div className="container mx-auto h-auto md:h-[440px] lg:h-[540px] flex flex-col justify-center lg:px-40 px-20">
-                <h1 className="text-center text-[60px] font-medium">We are WedMarks</h1>
-                <h2 className="text-center text-[50px] font-light">We bring</h2>
-                <h2 className="text-center text-[50px] font-medium">dream weddings</h2>
-                <h2 className="text-center text-[50px] font-light">to life!</h2>
+            <div className="container  mx-auto h-auto md:h-[440px] lg:h-[540px] flex flex-col justify-center  md:mt-20 lg:mt-0 mt-10">
+                <h1 className="text-center xl:text-[60px] lg:text-[50px] md:text-4xl text-3xl font-medium">
+                    We are WedMarks
+                </h1>
+                <h1 className="text-center xl:text-[50px] lg:text-4xl  text-3xl font-light lg:mt-6">We bring</h1>
+                <h1 className="text-center xl:text-[50px] lg:text-4xl  text-3xl font-medium lg:mt-6">dream weddings</h1>
+                <h1 className="text-center xl:text-[50px] lg:text-4xl  text-3xl font-light lg:mt-6">to life!</h1>
             </div>
             {/* section2 */}
-            <div className="flex flex-col items-center justify-center px-20 md:flex-row lg:px-40 md:gap-x-5">
+            <div className="flex flex-col items-center justify-center px-8 md:flex-row lg:px-40 md:gap-x-5">
                 <div className="md:w-1/2 lg:h-[627px] md:h-[500px] sm:h-[400px] flex flex-col justify-center lg:px-5">
-                    <h3 className="mb-4 text-3xl font-bold">About Us</h3>
+                    <h3 className="mt-6 mb-4 text-xl font-bold lg:text-3xl lg:mt-0">About Us</h3>
                     <p className="mb-4 lg:text-2xl">
                         At Wedmarks, we specialize in wedding event management, dedicated to creating unforgettable moments
                         for your dream wedding. With meticulous planning and flawless execution, our experienced team
                         ensures that your special day is a true reflection of your unique love story.
                     </p>
                 </div>
-                <div className="md:w-1/2 lg:h-[620px] md:h-[500px] sm:h-[400px] flex flex-col justify-center">
+                <div className="md:w-1/2 lg:h-[620px] md:h-[500px] sm:h-[400px] flex flex-col justify-center mt-2 lg:mt-0 sm:pb-14 md:pb-0">
                     <img src={image2} alt="" className="w-full h-auto" />
                 </div>
             </div>
@@ -64,9 +66,9 @@ const About = () => {
                 </div>
             </div>
             {/* section4 */}
-            <div className="flex flex-col items-center justify-center px-20 md:flex-row-reverse lg:px-40 md:gap-x-5">
+            <div className="flex flex-col items-center justify-center px-8 py-8 md:flex-row-reverse lg:px-40 md:gap-x-5 lg:py-0">
                 <div className="md:w-1/2 lg:h-[627px] md:h-[500px] sm:h-[400px] flex flex-col justify-center lg:px-5">
-                    <h3 className="mb-4 text-3xl font-bold">Whow We Are</h3>
+                    <h3 className="mt-6 mb-4 text-xl font-bold lg:text-3xl lg:mt-0">Whow We Are</h3>
                     <p className="mb-4 lg:text-2xl">
                         Wedmarks is a passionate team of experts in wedding event management. With meticulous planning and
                         flawless execution, we curate exceptional weddings that reflect your unique style. Trust us to
@@ -74,14 +76,14 @@ const About = () => {
                         creates unforgettable memories.
                     </p>
                 </div>
-                <div className="md:w-1/2 lg:h-[620px] md:h-[500px] sm:h-[400px] flex flex-col justify-center">
+                <div className="md:w-1/2 lg:h-[620px] md:h-[500px] sm:h-[400px] flex flex-col justify-center mt-2 lg:mt-0">
                     <img src={image2} alt="" className="w-full h-auto" />
                 </div>
             </div>
             {/* section4 */}
-            <div className="flex flex-col items-center justify-center px-20 md:flex-row lg:px-40 md:gap-x-5">
+            <div className="flex flex-col items-center justify-center px-8 py-8 md:flex-row lg:px-40 md:gap-x-5 lg:py-0">
                 <div className="md:w-1/2 lg:h-[627px] md:h-[500px] sm:h-[400px] flex flex-col justify-center lg:px-5">
-                    <h3 className="mb-4 text-3xl font-bold">What We Offer</h3>
+                    <h3 className="mb-4 text-xl font-bold lg:text-3xl">What We Offer</h3>
                     <p className="mb-4 lg:text-2xl">
                         At Wedmarks, we provide a full range of wedding services, from concept design and planning to venue
                         selection, vendor coordination, and flawless execution. With personalized solutions tailored to your
@@ -89,7 +91,7 @@ const About = () => {
                         ensuring a perfect day to cherish forever.
                     </p>
                 </div>
-                <div className="md:w-1/2 lg:h-[620px] md:h-[500px] sm:h-[400px] flex flex-col justify-center">
+                <div className="md:w-1/2 lg:h-[620px] md:h-[500px] sm:h-[400px] flex flex-col justify-center mt-2 lg:mt-0 sm:pb-14 md:pb-0">
                     <img src={image2} alt="" className="w-full h-auto" />
                 </div>
             </div>
